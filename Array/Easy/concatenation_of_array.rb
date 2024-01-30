6c124b3f5d375561310105cd533c29e40a627af6
Muhammad Mohsin Saeed <21135288+ITDevMohsin@users.noreply.github.com>
2024-01-30 10:37:02 -0500

    solving concatenation of array problem

---
+++ Array/Easy/concatenation_of_array.rb
@@ -0,0 +1,9 @@
+# Problem: https://leetcode.com/problems/concatenation-of-array/
+#
+# Topics: Array, Simulation
+#
+# @param {Integer[]} nums
+# @return {Integer[]}
+def get_concatenation(nums)
+  nums.concat(nums)
+end
